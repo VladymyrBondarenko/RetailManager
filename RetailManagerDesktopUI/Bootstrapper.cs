6222183973c4b303b39313fc547d579578cb490c
Бондаренко Владимир Валерьевич <vladymyr.bondarenko1@gmail.com>
2022-06-04 23:41:06 +0300

    Add WPF Project with MVVM + Database Project.
    Adds WPF ui project and configures it with
    Caliburn Microfor MVVN framework.
    Also includes the Simple Container DI system.
    Also adds empty Database Project.

---
+++ RetailManagerDesktopUI/Bootstrapper.cs
@@ -0,0 +1,55 @@
+﻿using Caliburn.Micro;
+using RetailManagerDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RetailManagerDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass && type.Name.EndsWith("ViewModel")).ToList()
+                .ForEach(type => 
+                    _container.RegisterPerRequest(type, type.ToString(), type)); // TODO: change wiring to interfaces
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
