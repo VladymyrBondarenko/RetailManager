85c4b9b907dbc3c1e6aaa9f120f58e3258e4f4ce
Бондаренко Владимир Валерьевич <vladymyr.bondarenko1@gmail.com>
2022-06-05 13:55:34 +0300

    Add WPF Login Form.

---
+++ RetailManagerDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailManagerDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set 
+            { 
+                _userName = value; 
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set 
+            { 
+                _password = value; 
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn
+        {
+            get
+            {
+                var output = false;
+
+                // TODO: do more thorough check on password and username later
+
+                if (!string.IsNullOrWhiteSpace(UserName) && !string.IsNullOrWhiteSpace(Password))
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
+
+        public void LogIn(string userName, string password)
+        {
+            Console.WriteLine();
+        }
+    }
+}
