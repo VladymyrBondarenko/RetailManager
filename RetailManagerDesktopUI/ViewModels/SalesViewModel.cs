163083a73d766d73c3b752919004e303c0d2379b
Бондаренко Владимир Валерьевич <vladymyr.bondarenko1@gmail.com>
2022-06-11 21:42:56 +0300

    Get Add to Cart working

--- RetailManagerDesktopUI/ViewModels/SalesViewModel.cs
+++ RetailManagerDesktopUI/ViewModels/SalesViewModel.cs
@@ -12,9 +12,10 @@ namespace RetailManagerDesktopUI.ViewModels
 {
     public class SalesViewModel : Screen
     {
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
         private BindingList<ProductModel> _products;
-        private BindingList<string> _cart;
+        private ProductModel _selectedProduct;
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
         private readonly IProductEndpoint _productEndpoint;
 
         public SalesViewModel(IProductEndpoint productEndpoint)
@@ -45,13 +46,27 @@ namespace RetailManagerDesktopUI.ViewModels
             }
         }
 
-        public BindingList<string> Card
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set 
             { 
                 _cart = value;
-                NotifyOfPropertyChange(() => Card);
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        public ProductModel SelectedProduct
+        {
+            get
+            {
+                return _selectedProduct;
+            }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -59,8 +74,13 @@ namespace RetailManagerDesktopUI.ViewModels
         {
             get 
             { 
-                // TODO: Replace with calculation
-                return "$0.00"; 
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                {
+                    subTotal += item.Product.RetailPrice * item.QuantityInCart;
+                }
+
+                return subTotal.ToString("C");
             }
         }
 
@@ -89,6 +109,7 @@ namespace RetailManagerDesktopUI.ViewModels
             { 
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -98,16 +119,40 @@ namespace RetailManagerDesktopUI.ViewModels
             {
                 var output = false;
 
-                // Make sure sm is selected
-                // Make sure there is an item quantity
+                if(SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
 
                 return output;
             }
         }
 
-        public void AddToCard()
+        public void AddToCart()
         {
+            var existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+            if(existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
 
+                // TODO: There should be a better way of refreshing the cart display
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                var cartItem = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(cartItem);
+            }
+
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+            NotifyOfPropertyChange(() => Cart);
         }
 
         public bool CanRemoveFromCart
@@ -124,7 +169,7 @@ namespace RetailManagerDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
